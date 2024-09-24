26b18db2d1ea60f0fc8d884cabec0106c9328640
Mikeyudex <miguel.yudex@gmail.com>
2024-09-23 19:35:11 -0500

    test_get_location_returns_side_effect

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,4 +1,6 @@
 import unittest
+import unittest.mock
+import requests
 from src.api_client import get_location
 from unittest.mock import patch
 
@@ -14,3 +16,30 @@ class ApiClientTest(unittest.TestCase):
         self.assertEqual(result.get("city"),"Mountain View")
 
         mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8") #Valida que el codigo este haciendo el llamado al endpoint correcto
+
+    @patch('src.api_client.requests.get') #el decorador patch inserta una variable dentro del metodo, en este caso mock_get
+    def test_get_location_returns_side_effect(self, mock_get):
+
+        #side_effect sirve para agregar comportamiento a nuestros llamados a un api, en este caso hay dos efectos, el primero es una excepción y el segundo una respuesta exitosa.
+        #La primera vez que se llame al metodo get_location fallará, en el segundo llamado será exitoso.
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Service Unavailable"),
+            unittest.mock.Mock(
+                status_code = 200,
+                json=lambda: {
+                    "countryName" : "United States of America",
+                    "regionName": "California",
+                    "cityName": "Mountain View"
+                }
+            )
+        ]
+
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")
+
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"),"United States of America")
+        self.assertEqual(result.get("region"),"California")
+        self.assertEqual(result.get("city"),"Mountain View")
+
+        
